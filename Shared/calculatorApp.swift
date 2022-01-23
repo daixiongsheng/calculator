3f6610cabc840b4acd20b9bf1a518fc2c85b24ac
daixiongsheng <daixiongsheng@gmail.com>
2022-01-23 21:05:37 +0800

    Initial Commit

---
+++ Shared/calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  calculatorApp.swift
+//  Shared
+//
+//  Created by daixiongsheng on 2022/1/23.
+//
+
+import SwiftUI
+
+@main
+struct calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
